904a0057a57ee640944cfd5fda5d064793e11216
Johnathon Frocillo <john.frocillo@gmail.com>
2020-01-30 16:38:09 -0500

    Initial Commit

---
+++ Homework3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework3
+//
+//  Created by Johnathon Frocillo on 1/30/20.
+//  Copyright © 2020 Johnathon Frocillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
